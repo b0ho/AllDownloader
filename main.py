c11be337da7cb0129676a94015810f202bb4d6a9
b0ho <ybg4828@gmail.com>
2019-03-02 15:07:05 +0900

    ui 폼 추가

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QAxContainer import *
+from PyQt5.QtGui import *
+
+# 메인 윈도우 폼
+form_class = uic.loadUiType("mainwindow.ui")[0]
+
+
+class MyWindow(QMainWindow, form_class):
+    # 초기화
+    def __init__(self):
+        super().__init__()
+        self.setUi()
+
+
+    # Ui 로드
+    def setUi(self):
+        self.setupUi(self)
+
+        # 버튼-위치 에 대한 이벤트 연결
+        self.btn_location.clicked.connect(self.btn_loc_clicked)
+
+
+
+    # 버튼-위치 이벤트
+    def btn_loc_clicked(self):
+        print("ss")
+        fname = QFileDialog.getOpenFileName(self)
+        self.label.setText(fname[0])
+
+
+
+# 프로그램 실행
+if __name__ == "__main__":
+    print("start")
+    app = QApplication(sys.argv)
+    myApp = MyWindow()
+    myApp.show()
+    app.exec_()
--- mainwindow.py
+++ mainwindow.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/ybg4828/work/AllDownloader/all/mainwindow.ui'
+# Form implementation generated from reading ui file 'D:/OneDrive - Chonnam National University/work/AllDownloader/mainwindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.12
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -17,25 +16,31 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 681, 431))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 654, 351))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.textEdit = QtWidgets.QTextEdit(self.verticalLayoutWidget)
+        self.textEdit.setObjectName("textEdit")
+        self.horizontalLayout_2.addWidget(self.textEdit)
         self.plainTextEdit = QtWidgets.QPlainTextEdit(self.verticalLayoutWidget)
-        self.plainTextEdit.setMinimumSize(QtCore.QSize(0, 300))
-        self.plainTextEdit.setMaximumSize(QtCore.QSize(16777215, 1677777))
         self.plainTextEdit.setObjectName("plainTextEdit")
-        self.verticalLayout.addWidget(self.plainTextEdit)
+        self.horizontalLayout_2.addWidget(self.plainTextEdit)
+        self.radioButton = QtWidgets.QRadioButton(self.verticalLayoutWidget)
+        self.radioButton.setObjectName("radioButton")
+        self.horizontalLayout_2.addWidget(self.radioButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        spacerItem = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout.addItem(spacerItem)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.label.setMinimumSize(QtCore.QSize(230, 0))
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        self.checkBox_2 = QtWidgets.QCheckBox(self.verticalLayoutWidget)
-        self.checkBox_2.setObjectName("checkBox_2")
-        self.horizontalLayout.addWidget(self.checkBox_2)
         self.toolButton = QtWidgets.QToolButton(self.verticalLayoutWidget)
         self.toolButton.setMinimumSize(QtCore.QSize(50, 0))
         self.toolButton.setObjectName("toolButton")
@@ -60,12 +65,17 @@ class Ui_MainWindow(object):
         self.verticalLayout.addLayout(self.horizontalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
         self.toolBar = QtWidgets.QToolBar(MainWindow)
-        self.toolBar.setMinimumSize(QtCore.QSize(0, 50))
+        self.toolBar.setMinimumSize(QtCore.QSize(700, 50))
+        self.toolBar.setInputMethodHints(QtCore.Qt.ImhNone)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
-        self.actionasdf = QtWidgets.QAction(MainWindow)
-        self.actionasdf.setObjectName("actionasdf")
+        self.action_2 = QtWidgets.QAction(MainWindow)
+        self.action_2.setObjectName("action_2")
+        self.action = QtWidgets.QAction(MainWindow)
+        self.action.setObjectName("action")
         self.toolBar.addSeparator()
+        self.toolBar.addAction(self.action_2)
+        self.toolBar.addAction(self.action)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -73,8 +83,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.radioButton.setText(_translate("MainWindow", "RadioButton"))
         self.label.setText(_translate("MainWindow", "다운로드 경로가 여기에 표시됩니다."))
-        self.checkBox_2.setText(_translate("MainWindow", "CheckBox"))
         self.toolButton.setText(_translate("MainWindow", "위치"))
         self.checkBox.setText(_translate("MainWindow", "자막"))
         self.comboBox.setItemText(0, _translate("MainWindow", "mp3"))
@@ -84,9 +94,8 @@ class Ui_MainWindow(object):
         self.comboBox.setItemText(4, _translate("MainWindow", "mp4[저화질]"))
         self.pushButton.setText(_translate("MainWindow", "다운로드 시작"))
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
-        self.actionasdf.setText(_translate("MainWindow", "asdf"))
-
-
+        self.action_2.setText(_translate("MainWindow", "옵션"))
+        self.action.setText(_translate("MainWindow", "도움말"))
 
 
 if __name__ == "__main__":
@@ -97,3 +106,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
