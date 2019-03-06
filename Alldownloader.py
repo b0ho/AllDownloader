7b9127fd155bd6cd9ed3e3ad439a832f41420e55
b0ho <ybg4828@gmail.com>
2019-03-06 17:02:54 +0900

    맥 윈도우 호환 다운로드 기능

--- Alldownloader.py
+++ Alldownloader.py
@@ -6,35 +6,42 @@ import csv
 import getpass
 import atexit
 import main_window
+from PyQt5 import uic
 
 csv_name = "option.csv"
 
+form_class = uic.loadUiType("mainwindow.ui")[0]
 
-class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
+#class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
+
+class Alldownloader(QMainWindow, form_class):
     # 옵션을 위한 변수들
     opt_loc = ""
     opt_sub = 0
     opt_format = ""
     options = []
+    os_name = 1
 
     # 초기화
     def __init__(self):
         print("초기화")
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.setOS()
         self.setOption()
         self.setUi()
 
         # 프로그램 종료 시 수행 기능
         def func_exit():
+            global opt_loc, opt_sub, opt_format
             print("프로그램 종료", opt_sub)
 
-            f = open("tmp", "w", encoding="utf-8", newline="")
+            os.remove(csv_name)
+            f = open(csv_name, "w", encoding="utf-8", newline="")
             wr = csv.writer(f)
             # 생성할 기본 옵션 값들
             wr.writerow([opt_loc, opt_sub, opt_format])
             f.close()
-            os.rename("tmp", csv_name)
 
             #
             f = open(csv_name, "r", encoding="utf-8")
@@ -46,9 +53,19 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
         # 프로그램 종료시 실행
         atexit.register(func_exit)
 
+    # 윈도우 인지 맥 인지 확인
+    def setOS(self):
+        global os_name
+        if os.name == 'nt':
+            os_name = 0
+            print("윈도우입니다.")
+        else:
+            os_name = 1
+            print("맥 입니다.")
+
     # 옵션 파일 생성 or 로드
     def setOption(self):
-        global opt_loc, opt_sub, opt_format, options
+        global opt_loc, opt_sub, opt_format, options, os_name
 
         # 옵션 파일이 있을 경우 로드
         if os.path.exists(csv_name):
@@ -58,27 +75,32 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
 
             for line in rdr:
                 options = line
-                print(line, options)
 
             opt_loc = options[0]
             opt_sub = options[1]
             opt_format = options[2]
 
-            print(opt_loc, opt_sub, opt_format)
+            print(options)
 
             f.close()
 
         # 옵션 파일이 없을 경우 생성
         else:
             print("옵션파일 없음 새로 생성")
+
+            loc_win = "C:/Users/" + getpass.getuser() + "/Downloads/"
+            loc_mac = "/users/" + getpass.getuser() + "/Downloads/"
+            loc = loc_win if (os_name == 0) else loc_mac
+
             f = open(csv_name, "w", encoding="utf-8", newline="")
             wr = csv.writer(f)
             # 생성할 기본 옵션 값들
-            wr.writerow(["/users/ybg4828/Downloads/", 0, "mp3"])
+            wr.writerow([loc, 0, "mp3"])
             f.close()
 
     # Ui 로드
     def setUi(self):
+        global opt_sub, opt_loc
         print("setUi 실행 - 가져온 옵션 값으로 프로그램을 세팅")
 
         # 자막 체크 여부확인 및 결정
@@ -89,6 +111,9 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
             print("opt_sub = ", opt_sub, "자막 옵션 꺼짐")
             self.chbx_subtitle.setChecked(False)
 
+        # 경로 확인
+        self.label_location.setText(opt_loc)
+
     # 툴바-옵션 이벤트
     def tbr_opt_clicked(self):
         print("옵션 화면 실행")
@@ -97,15 +122,13 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
     def tbr_help_clicked(self):
         print("도움말 화면 실행")
 
-    # 텍스트-URL 이벤트
-    def text_url_input(self):
-        print("url 입력됨")
-
     # 버튼-위치 이벤트
     def btn_loc_clicked(self):
+        global opt_loc
         print("fileDialog 오픈")
-        fname = QFileDialog.getOpenFileName(self)
-        self.label.setText(fname[0])
+        fname = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.label_location.setText(fname)
+        opt_loc = fname+"/"
 
     # 체크박스-자막 이벤트
     def chbx_sub_clicked(self):
@@ -119,7 +142,8 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
 
     # 콤보박스-포맷 이벤트
     def cmbx_format_choiced(self):
-        print("포맷 결정함")
+        print("포맷 결정함", self.cmbx_format.setText)
+
 
     # 버튼-다운시작 이벤트
     def btn_down_clicked(self, event):
@@ -130,18 +154,21 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
             "다운로드를 시작하시겠습니까?",
             QMessageBox.Yes | QMessageBox.No,
             QMessageBox.No,
-        )
+            )
         if reply == QMessageBox.Yes:
-
             self.func_down()
             # event.accept()
         else:
-            print("취소")
+            n = "https://www.youtube.com/watch?v=eTIOh7vhdN8"
             print(
                 "테스트",
                 os.system(
-                    'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "/Users/ybg4828/Downloads/%(title)s.%(ext)s" "https://www.youtube.com/watch?v=eTIOh7vhdN8"'
-                    # 'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "/Users/ybg4828/Downloads/%(title)s.%(ext)s" "https://www.youtube.com/watch?v=eTIOh7vhdN8"'
+
+                    'youtube-dl --extract-audio --audio-format mp3 -o "' + opt_loc + '%(title)s.%(ext)s" "' + n + '"'
+                    # 'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "'+opt_loc+'%(title)s.%(ext)s" "'+n+'"'
+                    # 'youtube-dl --extract-audio --audio-format mp3 -o "/Users/', getpass.getuser(),'/Downloads/%(title)s.%(ext)s" "', n, '"',
+                    # mac 'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "/Users/ybg4828/Downloads/%(title)s.%(ext)s" "https://www.youtube.com/watch?v=eTIOh7vhdN8"'
+
                 )
             )
             # event.ignore()
@@ -159,23 +186,21 @@ class Alldownloader(QMainWindow, main_window.Ui_MainWindow):
 
         # 가져온 url로부터 명령어 생성
         for n in urls:
-
-            url = 'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "/Users/' + getpass.getuser() + '/Downloads/%(title)s.%(ext)s" "' + n + '"'
+            url = 'youtube-dl --extract-audio --audio-format mp3 -o "' + opt_loc + '%(title)s.%(ext)s" "' + n + '"'
+            # 'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "'+opt_loc+'%(title)s.%(ext)s" "'+n+'"'
             print(url)
 
             print(
                 os.system(url)
-
 
                 #'youtube-dl --extract-audio --audio-format mp3 -o "/Users/',getpass.getuser(),'/Downloads/%(title)s.%(ext)s" "',n,'"',
                 #'youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "/Users/'+ getpass.getuser()+ '/Downloads/%(title)s.%(ext)s" "' + n+ '"'
                 # youtube-dl -f "bestvideo+bestaudio" --merge-output-format "mkv" -o "C:/Users/ybg48/Downloads/%(title)s.%(ext)s" --sub-lang ko ""
             )
             # https://www.youtube.com/watch?v=eTIOh7vhdN8
-
             # cmd = ["powershell.exe","Start-Process","youtube-dl","/Users/ybg4828/work/Alldownloader/youtube-dl-master/bin/youtube-dl","-Verb","runAs",]
-
             # print(subprocess.run(cmd, shell=True))
+
 
 
 # 프로그램 실행
